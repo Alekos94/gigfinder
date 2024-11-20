0163ea7103346454c1b731a404fb333634719f0d
george <georgeeburt@icloud.com>
2024-11-20 15:24:07 +0000

    feat: add unit tests for favourites-list, favourites-page and landing-page

---
+++ client/src/components/favourites-list/favourites-list.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { FavouritesList } from './favourites-list';
+import { mockFavouriteList } from '../../../tests/mocks/favouriteMocks';
+import { useNavigate } from 'react-router-dom';
+import '@testing-library/jest-dom'
+import '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
+describe('FavouritesList', () => {
+  it('should render the list of favourites', () => {
+    render(
+      <BrowserRouter>
+        <FavouritesList events={mockFavouriteList} />
+      </BrowserRouter>
+    )
+    expect(screen.getByText('Bon Giovi')).toBeInTheDocument();
+    expect(screen.getByText('The Watch Plays Genesis')).toBeInTheDocument();
+  });
+
+  it('should navigate tp /event-details when "More info" button is clicked', async () => {
+    const mockNavigate = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
+    render (
+      <BrowserRouter>
+        <FavouritesList events={mockFavouriteList} />
+      </BrowserRouter>
+    );
+    const moreInfoButton = screen.getAllByRole('button', { name: /more info/i })
+    await userEvent.click(moreInfoButton[0]);
+    expect(mockNavigate).toHaveBeenCalledWith(
+      '/event-details/',
+      expect.objectContaining({
+        state: expect.objectContaining({
+          event: expect.objectContaining({
+            _embedded: expect.objectContaining({
+              attractions: expect.any(Array),
+            }),
+          }),
+        }),
+      })
+    );
+  });
+});
---
+++ client/tests/mocks/favouriteMocks.ts
@@ -0,0 +1,599 @@
+export const mockFavouriteList = [
+  {
+    "eventId": "17GOvOG6GwcbvnM",
+    "eventDetails": {
+      "name": "Bon Giovi",
+      "type": "event",
+      "id": "17GOvOG6GwcbvnM",
+      "test": false,
+      "url": "https://www.ticketweb.uk/event/bon-giovi-the-brook-tickets/13682403?REFERRAL_ID=tmfeed",
+      "locale": "en-us",
+      "images": [
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_EVENT_DETAIL_PAGE_16_9.jpg",
+          "width": 205,
+          "height": 115,
+          "fallback": true
+        },
+        {
+          "ratio": "4_3",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_CUSTOM.jpg",
+          "width": 305,
+          "height": 225,
+          "fallback": true
+        },
+        {
+          "ratio": "3_2",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_TABLET_LANDSCAPE_3_2.jpg",
+          "width": 1024,
+          "height": 683,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_TABLET_LANDSCAPE_LARGE_16_9.jpg",
+          "width": 2048,
+          "height": 1152,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RECOMENDATION_16_9.jpg",
+          "width": 100,
+          "height": 56,
+          "fallback": true
+        },
+        {
+          "ratio": "3_2",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RETINA_PORTRAIT_3_2.jpg",
+          "width": 640,
+          "height": 427,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RETINA_LANDSCAPE_16_9.jpg",
+          "width": 1136,
+          "height": 639,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_TABLET_LANDSCAPE_16_9.jpg",
+          "width": 1024,
+          "height": 576,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RETINA_PORTRAIT_16_9.jpg",
+          "width": 640,
+          "height": 360,
+          "fallback": true
+        },
+        {
+          "ratio": "3_2",
+          "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_ARTIST_PAGE_3_2.jpg",
+          "width": 305,
+          "height": 203,
+          "fallback": true
+        }
+      ],
+      "distance": 3.98,
+      "units": "MILES",
+      "sales": {
+        "public": {
+          "startDateTime": "2024-06-15T19:30:00Z",
+          "startTBD": false,
+          "startTBA": false,
+          "endDateTime": "2024-11-23T22:00:00Z"
+        }
+      },
+      "dates": {
+        "start": {
+          "localDate": "2024-11-23",
+          "localTime": "21:00:00",
+          "dateTime": "2024-11-23T21:00:00Z",
+          "dateTBD": false,
+          "dateTBA": false,
+          "timeTBA": false,
+          "noSpecificTime": false
+        },
+        "timezone": "Europe/London",
+        "status": {
+          "code": "onsale"
+        },
+        "spanMultipleDays": false
+      },
+      "classifications": [
+        {
+          "primary": true,
+          "segment": {
+            "id": "KZFzniwnSyZfZ7v7nJ",
+            "name": "Music"
+          },
+          "genre": {
+            "id": "KnvZfZ7vAe6",
+            "name": "Undefined"
+          },
+          "subGenre": {
+            "id": "KZazBEonSMnZfZ7v6JI",
+            "name": "Undefined"
+          },
+          "type": {
+            "id": "KZAyXgnZfZ7v7l1",
+            "name": "Group"
+          },
+          "subType": {
+            "id": "KZFzBErXgnZfZ7vAAI",
+            "name": "Tribute Band"
+          },
+          "family": false
+        }
+      ],
+      "accessibility": {
+        "id": "accessibility"
+      },
+      "ageRestrictions": {
+        "legalAgeEnforced": false,
+        "id": "ageRestrictions"
+      },
+      "ticketing": {
+        "safeTix": {
+          "enabled": false,
+          "inAppOnlyEnabled": false
+        },
+        "allInclusivePricing": {
+          "enabled": false
+        },
+        "id": "ticketing"
+      },
+      "_links": {
+        "self": {
+          "href": "/discovery/v2/events/17GOvOG6GwcbvnM?locale=en-us"
+        },
+        "attractions": [
+          {
+            "href": "/discovery/v2/attractions/K8vZ917oKB7?locale=en-us"
+          }
+        ],
+        "venues": [
+          {
+            "href": "/discovery/v2/venues/KovZpZAt6t6A?locale=en-de"
+          }
+        ]
+      },
+      "_embedded": {
+        "venues": [
+          {
+            "name": "The Brook",
+            "type": "venue",
+            "id": "KovZpZAt6t6A",
+            "test": false,
+            "url": "https://www.ticketmaster.co.uk/the-brook-tickets-southampton/venue/416577",
+            "locale": "en-de",
+            "distance": 3.98,
+            "units": "MILES",
+            "postalCode": "SO17 3SD",
+            "timezone": "Europe/London",
+            "city": {
+              "name": "Southampton"
+            },
+            "country": {
+              "name": "Great Britain",
+              "countryCode": "GB"
+            },
+            "address": {
+              "line1": "466 Portswood Road"
+            },
+            "location": {
+              "longitude": "-1.38340900",
+              "latitude": "50.93284700"
+            },
+            "markets": [
+              {
+                "name": "All of United Kingdom",
+                "id": "201"
+              },
+              {
+                "name": "South",
+                "id": "203"
+              }
+            ],
+            "dmas": [
+              {
+                "id": 601
+              },
+              {
+                "id": 603
+              }
+            ],
+            "upcomingEvents": {
+              "ticketmaster": 45,
+              "_total": 45,
+              "_filtered": 0
+            },
+            "_links": {
+              "self": {
+                "href": "/discovery/v2/venues/KovZpZAt6t6A?locale=en-de"
+              }
+            }
+          }
+        ],
+        "attractions": [
+          {
+            "name": "Bon Giovi",
+            "type": "attraction",
+            "id": "K8vZ917oKB7",
+            "test": false,
+            "url": "https://www.ticketmaster.com/bon-giovi-tickets/artist/1875130",
+            "locale": "en-us",
+            "externalLinks": {
+              "twitter": [
+                {
+                  "url": "https://twitter.com/bongiovitribute"
+                }
+              ],
+              "facebook": [
+                {
+                  "url": "https://www.facebook.com/bongiovitributeband/"
+                }
+              ],
+              "homepage": [
+                {
+                  "url": "http://www.bongiovi.co.uk"
+                }
+              ]
+            },
+            "images": [
+              {
+                "ratio": "16_9",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_EVENT_DETAIL_PAGE_16_9.jpg",
+                "width": 205,
+                "height": 115,
+                "fallback": true
+              },
+              {
+                "ratio": "4_3",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_CUSTOM.jpg",
+                "width": 305,
+                "height": 225,
+                "fallback": true
+              },
+              {
+                "ratio": "3_2",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_TABLET_LANDSCAPE_3_2.jpg",
+                "width": 1024,
+                "height": 683,
+                "fallback": true
+              },
+              {
+                "ratio": "16_9",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_TABLET_LANDSCAPE_LARGE_16_9.jpg",
+                "width": 2048,
+                "height": 1152,
+                "fallback": true
+              },
+              {
+                "ratio": "16_9",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RECOMENDATION_16_9.jpg",
+                "width": 100,
+                "height": 56,
+                "fallback": true
+              },
+              {
+                "ratio": "3_2",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RETINA_PORTRAIT_3_2.jpg",
+                "width": 640,
+                "height": 427,
+                "fallback": true
+              },
+              {
+                "ratio": "16_9",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RETINA_LANDSCAPE_16_9.jpg",
+                "width": 1136,
+                "height": 639,
+                "fallback": true
+              },
+              {
+                "ratio": "16_9",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_TABLET_LANDSCAPE_16_9.jpg",
+                "width": 1024,
+                "height": 576,
+                "fallback": true
+              },
+              {
+                "ratio": "16_9",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_RETINA_PORTRAIT_16_9.jpg",
+                "width": 640,
+                "height": 360,
+                "fallback": true
+              },
+              {
+                "ratio": "3_2",
+                "url": "https://s1.ticketm.net/dam/c/ab4/6367448e-7474-4650-bd2d-02a8f7166ab4_106161_ARTIST_PAGE_3_2.jpg",
+                "width": 305,
+                "height": 203,
+                "fallback": true
+              }
+            ],
+            "classifications": [
+              {
+                "primary": true,
+                "segment": {
+                  "id": "KZFzniwnSyZfZ7v7nJ",
+                  "name": "Music"
+                },
+                "genre": {
+                  "id": "KnvZfZ7vAe6",
+                  "name": "Undefined"
+                },
+                "subGenre": {
+                  "id": "KZazBEonSMnZfZ7v6JI",
+                  "name": "Undefined"
+                },
+                "type": {
+                  "id": "KZAyXgnZfZ7v7l1",
+                  "name": "Group"
+                },
+                "subType": {
+                  "id": "KZFzBErXgnZfZ7vAAI",
+                  "name": "Tribute Band"
+                },
+                "family": false
+              }
+            ],
+            "upcomingEvents": {
+              "ticketmaster": 1,
+              "_total": 1,
+              "_filtered": 0
+            },
+            "_links": {
+              "self": {
+                "href": "/discovery/v2/attractions/K8vZ917oKB7?locale=en-us"
+              }
+            }
+          }
+        ]
+      }
+    },
+    "_id": "673dc849c6d8bd83fa061fe7",
+    "__v": 0
+  },
+  {
+    "eventId": "1AdFZb7GkBjJv_h",
+    "eventDetails": {
+      "name": "The Watch Plays Genesis",
+      "type": "event",
+      "id": "1AdFZb7GkBjJv_h",
+      "test": false,
+      "url": "https://www.ticketweb.uk/event/the-watch-plays-genesis-the-1865-tickets/13525353?REFERRAL_ID=tmfeed",
+      "locale": "en-us",
+      "images": [
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_EVENT_DETAIL_PAGE_16_9.jpg",
+          "width": 205,
+          "height": 115,
+          "fallback": true
+        },
+        {
+          "ratio": "3_2",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_RETINA_PORTRAIT_3_2.jpg",
+          "width": 640,
+          "height": 427,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_RETINA_PORTRAIT_16_9.jpg",
+          "width": 640,
+          "height": 360,
+          "fallback": true
+        },
+        {
+          "ratio": "4_3",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_CUSTOM.jpg",
+          "width": 305,
+          "height": 225,
+          "fallback": true
+        },
+        {
+          "ratio": "3_2",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_TABLET_LANDSCAPE_3_2.jpg",
+          "width": 1024,
+          "height": 683,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_RECOMENDATION_16_9.jpg",
+          "width": 100,
+          "height": 56,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_TABLET_LANDSCAPE_LARGE_16_9.jpg",
+          "width": 2048,
+          "height": 1152,
+          "fallback": true
+        },
+        {
+          "ratio": "3_2",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_ARTIST_PAGE_3_2.jpg",
+          "width": 305,
+          "height": 203,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_RETINA_LANDSCAPE_16_9.jpg",
+          "width": 1136,
+          "height": 639,
+          "fallback": true
+        },
+        {
+          "ratio": "16_9",
+          "url": "https://s1.ticketm.net/dam/c/fbc/b293c0ad-c904-4215-bc59-8d7f2414dfbc_106141_TABLET_LANDSCAPE_16_9.jpg",
+          "width": 1024,
+          "height": 576,
+          "fallback": true
+        }
+      ],
+      "distance": 5.09,
+      "units": "MILES",
+      "sales": {
+        "public": {
+          "startDateTime": "2024-02-16T10:00:00Z",
+          "startTBD": false,
+          "startTBA": false,
+          "endDateTime": "2024-11-24T17:00:00Z"
+        }
+      },
+      "dates": {
+        "start": {
+          "localDate": "2024-11-24",
+          "localTime": "19:30:00",
+          "dateTime": "2024-11-24T19:30:00Z",
+          "dateTBD": false,
+          "dateTBA": false,
+          "timeTBA": false,
+          "noSpecificTime": false
+        },
+        "timezone": "Europe/London",
+        "status": {
+          "code": "onsale"
+        },
+        "spanMultipleDays": false
+      },
+      "classifications": [
+        {
+          "primary": true,
+          "segment": {
+            "id": "KZFzniwnSyZfZ7v7nJ",
+            "name": "Music"
+          },
+          "genre": {
+            "id": "KnvZfZ7vAeA",
+            "name": "Rock"
+          },
+          "subGenre": {
+            "id": "KZazBEonSMnZfZ7v6F1",
+            "name": "Pop"
+          },
+          "type": {
+            "id": "KZAyXgnZfZ7v7nI",
+            "name": "Undefined"
+          },
+          "subType": {
+            "id": "KZFzBErXgnZfZ7v7lJ",
+            "name": "Undefined"
+          },
+          "family": false
+        }
+      ],
+      "accessibility": {
+        "id": "accessibility"
+      },
+      "ageRestrictions": {
+        "legalAgeEnforced": false,
+        "id": "ageRestrictions"
+      },
+      "ticketing": {
+        "safeTix": {
+          "enabled": false,
+          "inAppOnlyEnabled": false
+        },
+        "allInclusivePricing": {
+          "enabled": false
+        },
+        "id": "ticketing"
+      },
+      "_links": {
+        "self": {
+          "href": "/discovery/v2/events/1AdFZb7GkBjJv_h?locale=en-us"
+        },
+        "venues": [
+          {
+            "href": "/discovery/v2/venues/KovZ9177NC7?locale=en-de"
+          }
+        ]
+      },
+      "_embedded": {
+        "venues": [
+          {
+            "name": "Southampton 1865",
+            "type": "venue",
+            "id": "KovZ9177NC7",
+            "test": false,
+            "url": "https://www.ticketmaster.co.uk/southampton-1865-tickets-southampton/venue/435305",
+            "locale": "en-de",
+            "distance": 5.09,
+            "units": "MILES",
+            "postalCode": "SO14 3AR",
+            "timezone": "Europe/London",
+            "city": {
+              "name": "Southampton"
+            },
+            "country": {
+              "name": "Great Britain",
+              "countryCode": "GB"
+            },
+            "address": {
+              "line1": "Brunswick Square"
+            },
+            "location": {
+              "longitude": "-1.40046330",
+              "latitude": "50.89898110"
+            },
+            "markets": [
+              {
+                "name": "All of United Kingdom",
+                "id": "201"
+              },
+              {
+                "name": "South",
+                "id": "203"
+              }
+            ],
+            "dmas": [
+              {
+                "id": 601
+              },
+              {
+                "id": 603
+              }
+            ],
+            "boxOfficeInfo": {
+              "phoneNumberDetail": "Ticketmaster UK 0870 154 40 40, International 00 44 161 385 3500 Box office: 020 8980 3030",
+              "willCallDetail": "Collect from The Hippodrome during the 45 minutes before the show. Take the credit card and the Ticketmaster reference number."
+            },
+            "accessibleSeatingDetail": "The Hippodrome is accessible to those with special needs.",
+            "generalInfo": {
+              "generalRule": "No recording. No photography. No alcohol. No smoking in auditorium."
+            },
+            "upcomingEvents": {
+              "ticketmaster": 119,
+              "_total": 119,
+              "_filtered": 0
+            },
+            "ada": {
+              "adaPhones": "02380 222 605 ",
+              "adaCustomCopy": "To better accommodate your needs, Southampton 1865 has requested that all Accesisble Ticketing be solicited only through their representatives. Please contact a Southampton 1865 representative for further help with your ticket purchase.*   Advance ticket purchase may be required.\n\n* Box office information is subject to change.",
+              "adaHours": " t.b.c"
+            },
+            "_links": {
+              "self": {
+                "href": "/discovery/v2/venues/KovZ9177NC7?locale=en-de"
+              }
+            }
+          }
+        ]
+      }
+    },
+    "_id": "673dc851c6d8bd83fa061fe9",
+    "__v": 0
+  }
+];
